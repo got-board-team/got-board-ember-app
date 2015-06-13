4e3f05409eb168c0a8e31fab71a9ee730a55722d
leandrost <leandro.s.thimoteo@gmail.com>
2015-06-13 15:48:33 -0300

    Remove unecessary code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,13 +1,3 @@
-import JsonApiAdapter from "json-api-adapter";
-import JsonApiSerializer from "json-api-adapter";
-
-//export default DS.FixtureAdapter.extend({});
-console.log("adapter");
-//let adapter = JsonApiAdapter.extend({
-  //namespace: 'api/v1',
-  //host: 'http://localhost:3000',
-  //serializer: "JsonApiSerializer",
-//});
 let adapter = DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
   host: 'http://localhost:3000',
