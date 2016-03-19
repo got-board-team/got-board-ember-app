b8a4131c775fd4c89d13a67737ea35d7ddaffe4d
Rafael B. Tauil <rafael@tauil.com.br>
2016-03-18 23:03:50 -0300

    Trying to send google autorization token to api

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -4,10 +4,32 @@ import Torii from 'ember-simple-auth/authenticators/torii';
 const { service } = Ember.inject;
 
 export default Torii.extend({
-  torii: service('torii')// ,
-  // authenticate(options) {
-  //   return this._super(options).then(function (data) {
-  //     alert(`authorizationCode:\n${data.authorizationCode}\nprovider: ${data.provider}\nredirectUri: ${data.redirectUri}`);
-  //   });
-  // }
+  torii: service('torii'),
+
+  authenticate(options) {
+    var request = new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax('http://localhost:3000/countries', {
+        success: function(response) {
+          resolve(response);
+        },
+        error: function(reason) {
+          reject(reason);
+        }
+      });
+    });
+
+    return this._super(options).then(function (data) {
+      console.log(data);
+      console.log(`authorizationCode:\n${data.authorizationCode}\nprovider: ${data.provider}\nredirectUri: ${data.redirectUri}`);
+
+      request.then(function(response) {
+        // e.g render template from response
+        console.log('then response: ' + response);
+      }, function(error) {
+        // handle error (show error message, retry, etc.)
+        console.log('then error response: ');
+        console.log(error);
+      });
+    });
+  }
 });
